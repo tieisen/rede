bba99169c0df9a8483cbc4e6403e290b69ba8b02
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2026-02-24 10:58:18 -0300

    adicinada tarefa de inicializacao do banco sqlite

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,5 @@
 import os, subprocess, sys
+from src.rede.database.database import verificar_criar_banco,criar_tabelas
 
 VENV_DIR = "venv"
 
@@ -16,16 +17,23 @@ def venv_pip():
         "pip",
     )
 
+def init_db():
+    verificar_criar_banco()
+    criar_tabelas()
+
 def main():
     if not os.path.exists(VENV_DIR):
         print("Criando ambiente virtual...")
         subprocess.check_call([sys.executable, "-m", "venv", VENV_DIR])
 
-    print("Atualizando pip...")
-    subprocess.check_call([venv_python(), "-m", "pip", "install", "--upgrade", "pip"])
+        print("Atualizando pip...")
+        subprocess.check_call([venv_python(), "-m", "pip", "install", "--upgrade", "pip"])
 
-    print("Instalando dependências...")
-    subprocess.check_call([venv_pip(), "install", "-e", "."])
+        print("Instalando dependências...")
+        subprocess.check_call([venv_pip(), "install", "-e", "."])
+
+    print("Iniciando banco de dados...")
+    init_db()
 
     print("Ambiente pronto!")
 
