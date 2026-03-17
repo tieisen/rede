a1d7539b1c1a0c782fd990d8de0afb0eb2ec0d24
TI <ti@grupoeisen.com.br>
2026-03-17 13:37:02 -0300

    update

--- src/rede/services/rotina.py
+++ src/rede/services/rotina.py
@@ -20,25 +20,21 @@ class RotinaService():
 
         logger.info(f"Registrando dados de pagamento para companyNumber={companyNumber}, dataVendas={dataVendas}, nsu={nsu}")
         try:
-            logger.info("- Autenticando com as APIs...")
             if not self.snk_pgto.autenticar():
                 raise Exception("Falha na autenticação com a API Sankhya.")
             if not self.rede_venda.autenticar():
                 raise Exception("Falha na autenticação com a API Rede.")
             
-            logger.info("- Consultando dados de pagamento na API Rede...")
             dados_rede = self.rede_venda.consultar_vendas_parceladas(companyNumber=companyNumber,startDate=dataVendas,endDate=dataVendas,nsu=nsu)
             if 'message' in dados_rede:
                 raise Exception(dados_rede.get('message'))
             
-            logger.info("- Formatando payload de retorno...")
             payload_pgto = self.rede_venda.formatar_payload_consulta_vendas_parceladas()
             if not payload_pgto:
                 raise Exception("Falha ao formatar payload de registro.")
             retorno['dados'] = payload_pgto
             retorno['sucesso'] = True
             
-            logger.info("- pass")
         except Exception as e:
             msg = f"Erro ao registrar dados de pagamento: {str(e)}"
             retorno['mensagem'] = msg
@@ -60,13 +56,11 @@ class RotinaService():
         retorno:dict = {"sucesso": False, "mensagem": ""}
 
         try:
-            logger.info("- Autenticando com as APIs...")
             if not self.rede_venda.autenticar():
-                raise Exception("Falha na autenticação com a API Sankhya.")
-            if not self.snk_pgto.autenticar():
                 raise Exception("Falha na autenticação com a API Rede.")
-            
-            logger.info("- Buscando dados de pagamento na API Rede...")
+            if not self.snk_pgto.autenticar():
+                raise Exception("Falha na autenticação com a API Sankhya.")
+
             # Busca dados de pagamento na API Rede
             dados_pagamento_raw = self.rede_venda.consultar_pagamentos_oc(companyNumber=companyNumber,
                                                                           startDate=startDate,
@@ -77,7 +71,6 @@ class RotinaService():
                 return {"sucesso": True, "mensagem": f"Nenhum pagamento encontrado para o período especificado ({startDate.strftime('%d/%m/%Y')}-{endDate.strftime('%d/%m/%Y')})."}
             dados_pagamento = dados_pagamento_raw['content']['paymentsCreditOrders']
 
-            logger.info("- Buscando dados financeiros na API Sankhya...")
             # Busca dados financeiros na API Sankhya com base nos salesSummaryNumber dos pagamentos encontrados
             lista_salesumnum = [d.get('saleSummaryNumber') for d in dados_pagamento]
             if not lista_salesumnum:
@@ -87,17 +80,14 @@ class RotinaService():
             if not dados_financeiro:
                 raise Exception("Nenhum registro financeiro encontrado para os salesSummaryNumber")
 
-            logger.info("- Formatando payload de atualização para a API Sankhya...")
             # Formata payload de atualização para a API Sankhya com base nos dados de pagamento e financeiro encontrados
             if not self.snk_pgto.formatar_payload_pagamento(dados_pagamento=dados_pagamento, dados_financeiro=dados_financeiro):
                 raise Exception("Falha ao formatar payload financeiro.")
 
-            logger.info("- Atualizando dados financeiros na API Sankhya...")
             upd_pgto_snk = self.snk_pgto.atualizar()
             retorno['sucesso'] = upd_pgto_snk
             if not upd_pgto_snk:
                 raise Exception("Falha ao atualizar dados financeiro.")
-            logger.info("- pass")
         except Exception as e:
             msg = f"Erro ao atualizar dados financeiro: {str(e)}"
             retorno['mensagem'] = msg
