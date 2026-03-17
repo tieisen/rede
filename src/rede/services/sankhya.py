a6b768be89b29b636e4b55fae63149ccb38ee5b8
TI <ti@grupoeisen.com.br>
2026-03-17 14:02:02 -0300

    update

--- src/rede/services/sankhya.py
+++ src/rede/services/sankhya.py
@@ -1,5 +1,5 @@
-import os, requests, json
-from datetime import datetime, timedelta
+import os, requests
+from datetime import datetime
 from dotenv import load_dotenv
 from src.rede.utils.log import set_logger
 from src.rede.services.token import TokenService
@@ -698,9 +698,7 @@ class PagamentoService(AutenticacaoService):
         }
         headers = { "Authorization":f"Bearer {token}" }
 
-        logger.info(f"Enviando headers de atualização para a API Sankhya: {headers}")
-        logger.info(f"Enviando payload de atualização para a API Sankhya: {payload_send}")        
-
+        logger.info(f"Enviando payload de atualização para a API Sankhya: {payload_send}")
         try:
             res = requests.post(
                 url=url,
